52142ec7418122ebc7de3c5cb2a5d98db449c959
Iraildo Silva <iraildosilva1999@gmail.com>
2023-11-08 20:50:01 -0300

    feat: create Stock Product interface

--- src/pages/StockLayout/index.tsx
+++ src/pages/StockLayout/index.tsx
@@ -1,5 +1,22 @@
+import NavOption from '@/components/NavOption'
+import TableContainer from './TableContainer.tsx'
+import useStock from '@/hooks/useStock.tsx'
+
 export default function StockLayout() {
-	return (
-		<h1>StockLayout</h1>
-	)
+  const { products } = useStock()
+
+  return (
+    <div>
+      <h1 className="text-5xl font-light mb-6">Stock Products</h1>
+
+      <nav className="border-b-2 border-gray-400 p-1">
+        <NavOption active path={'/products/new'}>
+          Listar Produtos
+        </NavOption>
+        <NavOption path={'/products/new'}>Novo Produto</NavOption>
+      </nav>
+
+      <TableContainer products={products} />
+    </div>
+  )
 }
