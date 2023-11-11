efd936510710baafc14032e48bff2f53ff096ef0
Iraildo Silva <iraildosilva1999@gmail.com>
2023-11-11 11:38:31 -0300

    refactor: ListProduct fetch API

--- src/pages/ListProduct/index.tsx
+++ src/pages/ListProduct/index.tsx
@@ -1,14 +1,31 @@
+import { Link, useParams } from 'react-router-dom'
+import { productService } from '@/services/ProductService'
+import { useEffect, useState } from 'react'
+
 import Container from '@/components/Container'
 import { Button } from '@/components/ui/button'
-import useStock from '@/hooks/useStock'
-import { Link, useParams } from 'react-router-dom'
+import { MappedProduct } from '@/@types/MappedProduct'
 
 export default function ListProduct() {
-  const { products } = useStock()
+  const [product, setProduct] = useState<MappedProduct>({} as MappedProduct)
 
   const { id } = useParams()
 
-  const product = products.find((product) => product.id === id)
+  useEffect(() => {
+    async function loadProduct() {
+      try {
+        const product = await productService.getProductById(id!)
+
+        setProduct(product)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    loadProduct()
+  }, [id])
+
+  console.log(product)
 
   return (
     <div className="mt-4">
@@ -25,9 +42,9 @@ export default function ListProduct() {
       </div>
 
       <div className="flex w-full max-w-3xl gap-4 mt-8">
-        <Container content={`Categoria: ${product?.category.name}`} />
+        <Container content={`Categoria: ${product?.category?.name}`} />
         <Container content={`Quantidade em estoque: ${product?.quantity}`} />
-        <Container content={`Preço: R$${399},00`} />
+        <Container content={`Preço: R$${product?.price},00`} />
       </div>
 
       <span className="mt-8 block">{product?.description}</span>
