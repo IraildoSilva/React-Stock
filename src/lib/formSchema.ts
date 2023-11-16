8e196c8bab161e684baf63414b67b4372b1b0d20
Iraildo Silva <iraildosilva1999@gmail.com>
2023-11-16 09:44:56 -0300

    refactor: formSchema validation

--- src/lib/formSchema.ts
+++ src/lib/formSchema.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod'
 
 export const formSchema = z.object({
-  name: z.string(),
-  quantity: z.string(),
-  price: z.string(),
+  name: z.string().refine((data) => data.length > 0, {
+    message: 'O nome do produto não pode ser vazio',
+  }),
+  quantity: z.string().refine((data) => data !== '0' && data.length > 0, {
+    message: 'Por favor insira uma quantidade',
+  }),
+  price: z.string().refine((data) => data !== '0' && data.length > 0, {
+    message: 'Por favor insira o preço',
+  }),
   categoryId: z.string().uuid(),
   description: z.string(),
 })
